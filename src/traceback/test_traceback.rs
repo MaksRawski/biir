5e50a7cce3ffd3b3582746152892d0c42e5d4cac
Maks Rawski <maksymilian.rawski@tutanota.com>
2021-06-19 23:26:53 +0200

    use proptest

--- src/traceback/test_traceback.rs
+++ src/traceback/test_traceback.rs
@@ -1,5 +1,6 @@
 use super::*;
 use test_case::test_case;
+use proptest::prelude::*;
 
 #[test_case(0,   0, 0, "TEST 1"; "first char of the first line")]
 #[test_case(5,   0, 5, "TEST 1"; "last char of the first line")]
@@ -18,26 +19,24 @@ fn test_current_line(
     assert_eq!(current_line, expected_line);
 }
 
-// TODO: we could use quickcheck here!!!
-#[test_case(0, "Test 123"; "first char")]
-#[test_case(4, "Test 123"; "space")]
-#[test_case(7, "Test 123"; "last char")]
-#[test_case(3, "pchnąć w tę łódź jeża lub ośm skrzyń fig"; "special chars")]
-fn test_highlighting(index: usize, test_text: &str) {
-    let current_char_red = test_text.chars().nth(index).unwrap().to_string().red();
+proptest!{
+    #[test]
+    fn test_highlighting(index: usize, test_text: String) {
+        let current_char_red = test_text.chars().nth(index).unwrap().to_string().red();
 
-    let output = Traceback::highlight_current_char_in_line(test_text, index);
+        let output = Traceback::highlight_current_char_in_line(&test_text, index);
 
-    // each colored char takes 10 chars
-    // if we collect to String it will be stored as 10 chars
-    let letter_supposed_to_be_red = output.chars().skip(index).take(10).collect::<String>();
+        // each colored char takes 10 chars
+        // if we collect to String it will be stored as 10 chars
+        let letter_supposed_to_be_red = output.chars().skip(index).take(10).collect::<String>();
 
-    assert_eq!(letter_supposed_to_be_red, current_char_red.to_string());
+        assert_eq!(letter_supposed_to_be_red, current_char_red.to_string());
 
-    // assert that there is only one occurence of colored char
-    // to do that we check if final string's length
-    // is just 9 chars longer than the test_text
-    assert_eq!(output.len(), test_text.len() + 9);
+        // assert that there is only one occurence of colored char
+        // to do that we check if final string's length
+        // is just 9 chars longer than the test_text
+        assert_eq!(output.len(), test_text.len() + 9);
 
-    // TODO: assert that there is only single letter red
+        // TODO: assert that there is only single letter red
+    }
 }

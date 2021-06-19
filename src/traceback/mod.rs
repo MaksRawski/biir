8fc87490b4f9c9e7da342b2090ff5dfbbdc83d37
Maks Rawski <maksymilian.rawski@tutanota.com>
2021-06-19 19:07:15 +0200

    add more testcases

--- src/traceback/mod.rs
+++ src/traceback/mod.rs
@@ -5,19 +5,18 @@ pub struct Traceback;
 impl Traceback{
     /// returns info about current line as a tuple:
     /// (line_nr, char_nr, current_line)
+    /// indexes from 0
     fn current_line<'a>(program: &'a str, program_counter: usize) -> (usize, usize, &'a str){
-        let line_nr = &program[0..program_counter].lines().count();
-        let chars_before: &usize = &program.lines()
-            .take(*line_nr)
+        let line_nr = &program[0..program_counter].lines().count() - 1;
+        let current_line = &program.lines().nth(line_nr).unwrap();
+
+        let chars_before_current_line: &usize = &program.lines()
+            .take(line_nr)
             .fold(0, |sum, l| sum + l.chars().count());
-        println!("chars_before = {}, line_nr = {}", chars_before, line_nr);
-        println!("pc = {}", program_counter);
 
-        let char_nr = program_counter - chars_before;
-        let current_line = &program.lines().nth(line_nr - 1).unwrap();
-        println!("current line = {}", current_line);
+        let index_of_char_in_current_line = program_counter - chars_before_current_line;
 
-        (*line_nr, char_nr, current_line)
+        (line_nr, index_of_char_in_current_line, current_line)
     }
 
     /// returns entire line but with the current char red
--- src/traceback/test_traceback.rs
+++ src/traceback/test_traceback.rs
@@ -1,11 +1,38 @@
 use super::*;
 
 #[test]
-fn test_current_line(){
+fn test_current_line_0(){
+    let (line_nr, char_nr, current_line) = Traceback::current_line("TEST 1\nTEST 2", 0);
+
+    assert_eq!(line_nr, 0);
+    assert_eq!(char_nr, 0);
+    assert_eq!(current_line, "TEST 1");
+}
+
+#[test]
+fn test_current_line_1(){
     let (line_nr, char_nr, current_line) = Traceback::current_line("TEST 1\nTEST 2", 5);
 
-    assert_eq!(line_nr, 2);
-    assert_eq!(char_nr, 1);
+    assert_eq!(line_nr, 0);
+    assert_eq!(char_nr, 5);
+    assert_eq!(current_line, "TEST 1");
+}
+
+#[test]
+fn test_current_line_2(){
+    let (line_nr, char_nr, current_line) = Traceback::current_line("TEST 1\nTEST 2", 6);
+
+    assert_eq!(line_nr, 1);
+    assert_eq!(char_nr, 0);
+    assert_eq!(current_line, "TEST 2");
+}
+
+#[test]
+fn test_current_line_3(){
+    let (line_nr, char_nr, current_line) = Traceback::current_line("TEST 1\nTEST 2", 11);
+
+    assert_eq!(line_nr, 1);
+    assert_eq!(char_nr, 5);
     assert_eq!(current_line, "TEST 2");
 }
 

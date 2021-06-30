2e97f01d7785a7e753af7dde182ae790644e1aca
Maks Rawski <maksymilian.rawski@tutanota.com>
2021-06-30 17:57:50 +0200

    bring integration tests out of the extra module

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,3 +1,4 @@
+#[cfg(test)]
 extern crate biir;
 
 use std::fs;
@@ -5,33 +6,31 @@ use std::fs;
 use biir::parser::Parser;
 use biir::utils::Output;
 
-#[cfg(test)]
-mod example_programs {
-    use super::*;
-
-    #[test]
-    fn test_programs() {
-        for f in fs::read_dir("./tests/programs").unwrap() {
-            let f = f.unwrap();
-            let program = fs::read_to_string(f.path()).unwrap();
-            let filename = f.file_name().into_string().unwrap();
-
-            test_program(&program, filename);
-        }
+#[test]
+fn test_example_programs() {
+    for f in fs::read_dir("./tests/programs").unwrap() {
+        let f = f.unwrap();
+        let program = f.path();
+        test_program(program);
     }
-
-    fn test_program(program: &str, name: String) {
-        dbg!(name);
-        let mut parser = Parser::new();
-        parser.output = Output::Vector(Vec::new());
-
-        // expected output is in the first line
-        let expected_output = program.lines().next().unwrap().replace("\\n", "\n");
-
-        assert_eq!(parser.execute(&program, false, false), Ok(()));
-        assert_eq!(format!("[{}]", parser.output.read()), expected_output);
-    }
-
-    // TODO: test program with newlines
-    // TODO: test program with windows style (\r\n) newlines
 }
+
+fn test_program(program: std::path::PathBuf) {
+    dbg!(program.file_name().unwrap());
+    let mut parser = Parser::new();
+    parser.output = Output::Vector(Vec::new());
+
+    // expected output is in the first line
+    let expected_output = fs::read_to_string(&program)
+        .unwrap()
+        .lines()
+        .next()
+        .unwrap()
+        .replace("\\n", "\n");
+
+    assert_eq!(parser.run(&program, false, false), Ok(()));
+    assert_eq!(format!("[{}]", parser.output.read()), expected_output);
+}
+
+// TODO: test program with newlines
+// TODO: test program with windows style (\r\n) newlines

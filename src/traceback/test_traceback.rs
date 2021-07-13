97b8bddda67744b999d8fdeb75074f73662d2bb5
Maks Rawski <maksymilian.rawski@tutanota.com>
2021-07-14 00:10:25 +0200

    add tests for highlighting

--- src/traceback/test_traceback.rs
+++ src/traceback/test_traceback.rs
@@ -2,6 +2,9 @@ use super::*;
 use test_case::test_case;
 use proptest::prelude::*;
 
+use crate::unicodes::*;
+use unicode_segmentation::UnicodeSegmentation;
+
 // Unfortunetly we can't just put those test_cases
 // above definitions as they're inside impl block.
 // Also moving it all into module instead would require
@@ -14,8 +17,11 @@ use proptest::prelude::*;
 #[test_case("ABC\nDEFG\nHI", 6, 1)]
 #[test_case("ABC\nDEFG\nHI", 7, 2)]
 #[test_case("ABC\nDEFG\nHI", 8, 2)]
-fn test_line_nr(program: &str, program_counter: usize, expected_line_nr: usize){
-    assert_eq!(Traceback::line_number(program, program_counter), Ok( expected_line_nr) );
+fn test_line_nr(program: &str, program_counter: usize, expected_line_nr: usize) {
+    assert_eq!(
+        Traceback::line_number(program, program_counter),
+        Ok(expected_line_nr)
+    );
 }
 
 #[test_case("ABC\nDEFG\nHI", 0, 0, 0)]
@@ -24,8 +30,11 @@ fn test_line_nr(program: &str, program_counter: usize, expected_line_nr: usize){
 #[test_case("ABC\nDEFG\nHI", 1, 6, 3)]
 #[test_case("ABC\nDEFG\nHI", 2, 7, 0)]
 #[test_case("ABC\nDEFG\nHI", 2, 8, 1)]
-fn test_char_nr(program: &str, line_nr: usize, program_counter: usize, expected_char_nr: usize){
-    assert_eq!(Traceback::char_number(program, line_nr, program_counter), expected_char_nr);
+fn test_char_nr(program: &str, line_nr: usize, program_counter: usize, expected_char_nr: usize) {
+    assert_eq!(
+        Traceback::char_number(program, line_nr, program_counter),
+        expected_char_nr
+    );
 }
 
 #[test_case("😎BC\n🥳EFG\nHI", 0, 0)]
@@ -34,8 +43,11 @@ fn test_char_nr(program: &str, line_nr: usize, program_counter: usize, expected_
 #[test_case("😎BC\n🥳EFG\nHI", 6, 1)]
 #[test_case("😎BC\n🥳EFG\nHI", 7, 2)]
 #[test_case("😎BC\n🥳EFG\nHI", 8, 2)]
-fn test_line_nr_unicodes(program: &str, program_counter: usize, expected_line_nr: usize){
-    assert_eq!(Traceback::line_number(program, program_counter), Ok( expected_line_nr) );
+fn test_line_nr_unicodes(program: &str, program_counter: usize, expected_line_nr: usize) {
+    assert_eq!(
+        Traceback::line_number(program, program_counter),
+        Ok(expected_line_nr)
+    );
 }
 
 #[test_case("😎BC\n🥳EFG\nHI", 0, 0, 0)]
@@ -44,32 +56,48 @@ fn test_line_nr_unicodes(program: &str, program_counter: usize, expected_line_nr
 #[test_case("😎BC\n🥳EFG\nHI", 1, 6, 3)]
 #[test_case("😎BC\n🥳EFG\nHI", 2, 7, 0)]
 #[test_case("😎BC\n🥳EFG\nHI", 2, 8, 1)]
-fn test_char_nr_unicodes(program: &str, line_nr: usize, program_counter: usize, expected_char_nr: usize){
-    assert_eq!(Traceback::char_number(program, line_nr, program_counter), expected_char_nr);
+fn test_char_nr_unicodes(
+    program: &str,
+    line_nr: usize,
+    program_counter: usize,
+    expected_char_nr: usize,
+) {
+    assert_eq!(
+        Traceback::char_number(program, line_nr, program_counter),
+        expected_char_nr
+    );
 }
 
-proptest!{
-    #[test]
+#[test_case( 0, "a"   => format!("{}", "a".red())  ; "single character")]
+#[test_case( 0, "𝚨"   => format!("{}", "𝚨" .red()) ; "unicode character")]
+#[test_case( 0, "abc" => format!("{}bc", "a".red()) )]
+#[test_case( 1, "abc" => format!("a{}c", "b".red()) )]
+#[test_case( 2, "abc" => format!("ab{}", "c".red()) )]
+#[test_case( 12, "pchnąć w tę łódź jeża lub ośm skrzyń fig"
+    => format!("pchnąć w tę {}ódź jeża lub ośm skrzyń fig", "ł".red()) ; "long string with non ascii characters")]
+fn test_highlighting(index: usize, test_text: &str) -> String {
+    let test_text = &string_to_unicode_string(test_text);
+    Traceback::highlight_current_char_in_line(test_text, index).unwrap()
+}
+
+proptest! {
+    // TODO: this gives false results
+    // 𝚨 is actaully handled correctly
     #[ignore]
-    fn test_highlighting(index: usize, test_text: String) {
-        // skip empty strings as they're supposed to return Err
-        if test_text.len() == 0 { return Ok( () ) }
+    #[test]
+    fn fuzz_highlighting(index: usize, test_text: String){
+        let test_text = string_to_unicode_string(&test_text);
+        let output = Traceback::highlight_current_char_in_line(&test_text, index);
 
-        let output = Traceback::highlight_current_char_in_line(&test_text, index).unwrap();
+        if test_text.len() == 0 {
+            assert!(output.is_err());
+            return Ok( () );
+        }
+        let output = output.unwrap();
 
-        let current_char_red = test_text.chars().nth(index).unwrap();
+        let current_char = test_text.iter().nth(index).unwrap();
+        let index_of_colored_char = output.find(current_char).unwrap();
 
-        // each colored char takes 10 chars
-        // if we collect to String it will be stored as 10 chars
-        let letter_supposed_to_be_red = output.chars().skip(index).take(10).collect::<String>();
-
-        assert_eq!(letter_supposed_to_be_red, current_char_red.to_string());
-
-        // assert that there is only one occurence of colored char
-        // to do that we check if final string's length
-        // is just 9 chars longer than the test_text
-        assert_eq!(output.len(), test_text.len() + 9);
-
-        // TODO: assert that there is only single letter red
+        assert_eq!(index_of_colored_char, index);
     }
 }
